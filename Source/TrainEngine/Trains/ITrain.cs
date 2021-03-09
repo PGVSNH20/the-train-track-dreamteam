9ca30f8fb2c1da5a61989a1b101a69d7d5e5b6b1
Edgar Runnman <edgar.runnman@gmail.com>
2021-03-09 14:54:44 +0100

    commit trainClasses

---
+++ Source/TrainEngine/Trains/ITrain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainEngine.Trains
+{
+    internal interface ITrain
+    {
+        public int Id { set; }
+        string Name { set; }
+        int MaxSpeed { set; }
+        bool Operated { set; }
+        TrainPosition Position { set; }
+    }
+}
---
+++ Source/TrainEngine/Trains/TrainPosition.cs
@@ -0,0 +1,9 @@
+﻿namespace TrainEngine.Trains
+{
+    public class TrainPosition
+    {
+        private bool IsMoving { set; get; }
+        private string CurrentDestination { set; get; }
+        private bool IsAtStation { set; get; }
+    }
+}
