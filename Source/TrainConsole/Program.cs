428a76ee128aaa36539e463648fb1d64e3b7a56a
Isak Kavallin Johansson <79911856+isakKavallin@users.noreply.github.com>
2021-03-10 20:33:35 +0100

    commit

--- Source/TrainConsole/Program.cs
+++ Source/TrainConsole/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using TrainEngine;
 using System.IO;
+using System.Threading;
 
 namespace TrainConsole
 {
@@ -7,7 +9,6 @@ namespace TrainConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Train track!");
             // Step 1:
             // Parse the traintrack (Data/traintrack.txt) using ORM (see suggested code)
             // Parse the trains (Data/trains.txt)
@@ -15,6 +16,19 @@ namespace TrainConsole
             // Step 2:
             // Make the trains run in treads
 
+
+            // Read the file and display it line by line.  
+
+            /*TrackOrm trackOrm = new TrackOrm();
+            var track = trackOrm.ParseTrackDescription(Properties.Resources.traintrack1);
+            Console.WriteLine(track.NumberOfTrackParts);
+            Console.ReadKey();*/
+            
+            TrackOrm newTrack = new TrackOrm();
+            newTrack.ParseTrackDescription(Properties.Resources.traintrack3);
+
+            
+            Console.ReadKey();
 
         }
     }
--- Source/TrainEngine/TrackOrm.cs
+++ Source/TrainEngine/TrackOrm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TrainEngine
 {
@@ -6,7 +7,56 @@ namespace TrainEngine
     {
         public TrackDescription ParseTrackDescription(string track)
         {
-            throw new NotImplementedException();
+            string aLine;
+            TrackDescription trackNumber = new TrackDescription();
+            char[][] trackMap = new char[7][];
+            for (int i = 0; i < 7; i++)
+            {
+                trackMap[i] = new char[50];
+            }
+
+            StringReader strReader = new StringReader(track);
+            while (true)
+            {
+                int i = new int();
+                int line = new int();
+                aLine = strReader.ReadLine();
+                if (aLine != null)
+                {
+                    foreach (var c in aLine)
+                    {
+                        Console.WriteLine(line);
+                        trackMap[line][i] = c;
+                        i++;
+                    }
+                    line++;
+                    i = 0;
+                }
+                else
+                {
+                    break;
+                }
+                Console.WriteLine(trackMap[0][20]);
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                int length = trackMap[i].Length;
+
+                for (int a = 0; a < length; a++)
+                {
+                    var trackChar = trackMap[i][a];
+                    Console.Write(i + " " + a);
+                    Console.Write(trackChar);
+                    Console.ReadKey();
+                }
+                
+                Console.WriteLine();
+            }
+            
+            trackNumber.NumberOfTrackParts++;
+            return trackNumber;
+            
         }
     }
 }
