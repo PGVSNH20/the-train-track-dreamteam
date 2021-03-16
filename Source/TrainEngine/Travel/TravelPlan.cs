67f0314d356c5554399e271e79c4e98e5290756e
Elsa Matteoni <elsa.matteoni@hotmail.com>
2021-03-16 14:11:49 +0100

    commit

--- Source/TrainEngine/Travel/TravelPlan.cs
+++ Source/TrainEngine/Travel/TravelPlan.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.Json;
 using TrainEngine.Tracks;
 using TrainEngine.Trains;
 
@@ -26,20 +28,30 @@ namespace TrainEngine.Travel
             throw new NotImplementedException();
         }
 
-        public ITravelPlan ArriveAt(int stationId, TimeSpan ariveTime)
+        public ITravelPlan ArriveAt(int stationId, string ariveTime)
         {
             TripStop tripstop = new TripStop();
             tripstop.StationId = stationId;
+            tripstop.ArrivalTime = TimeSpan.Parse(ariveTime);
+            tripstop.TrainId = Train.Id;
+            TimeSpan? departureTime = tripstop.ArrivalTime + TimeSpan.Parse("0:05");
+            tripstop.DepartureTime = departureTime;
+            TimeTable.Add(tripstop);
+            return this;
         }
 
         public ITravelPlan GenerateNewPlan(string fileName)
         {
-            throw new NotImplementedException();
+            string jsonString = JsonSerializer.Serialize(TimeTable);
+            File.WriteAllText($"Data/{fileName}.json", jsonString);
+            return this;
         }
 
         public ITravelPlan SettActualTrain(int trainId)
         {
-            throw new NotImplementedException();
+            TrainsOrm trainsOrm = new TrainsOrm();
+            Train = trainsOrm.Trains.Find(t => t.Id == trainId);
+            return this;
         }
 
         public void Simulate(string fakeClock, int timeFastForward)
@@ -49,7 +61,14 @@ namespace TrainEngine.Travel
 
         public ITravelPlan StartAt(int stationId, string departureTime)
         {
-            throw new NotImplementedException();
+            TripStop tripstop = new TripStop();
+            tripstop.StationId = stationId;
+            tripstop.DepartureTime = TimeSpan.Parse(departureTime);
+            tripstop.TrainId = Train.Id;
+            TimeSpan? arriveTime = tripstop.DepartureTime - TimeSpan.Parse("0:05");
+            tripstop.ArrivalTime = arriveTime;
+            TimeTable.Add(tripstop);
+            return this;
         }
     }
 }
